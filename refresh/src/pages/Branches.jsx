e99e0dc408ce6d73fe7cd5eb3858b6df1cb0ea34
Nalish <sharonnaliaka@gmail.com>
2026-01-27 12:20:47 +0300

    branch url

--- refresh/src/pages/Branches.jsx
+++ refresh/src/pages/Branches.jsx
@@ -13,7 +13,7 @@ const Branches = () => {
 
   const fetchBranches = async () => {
     try {
-      const response = await axios.get("http://localhost:3001/api/branch", {
+      const response = await axios.get("https://refresh-backend-v9ti.onrender.com/api/branch", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
