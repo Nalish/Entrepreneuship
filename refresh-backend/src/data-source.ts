6a96732aff2cf4e35d53175c74f17cf9d06f6c5a
Nalish <sharonnaliaka@gmail.com>
2026-01-27 10:13:54 +0300

    database

--- refresh-backend/src/data-source.ts
+++ refresh-backend/src/data-source.ts
@@ -1,24 +1,11 @@
-import "reflect-metadata";
-import dotenv from "dotenv";
 import { DataSource } from "typeorm";
-import path from "path"
-
-dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  synchronize: false, // ALWAYS false when using migrations
+  url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  synchronize: true, // only for development
   logging: false,
-
-  // ✅ Load entities by path (no imports)
-  entities: [path.join(__dirname, "/entities/**/*.ts")],
-
-  // ✅ Load migrations by path
-  migrations: ["dist/migrations/**/*.js"],
+  entities: ["dist/entities/*.js"],
+  migrations: ["dist/migrations/*.js"],
 });
