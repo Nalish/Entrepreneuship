bcbbd59e86024c1dfe3fe0f41c67e8f3a2eb2183
Nalish <sharonnaliaka@gmail.com>
2026-01-27 16:05:59 +0300

    app.jsx

--- refresh/src/App.jsx
+++ refresh/src/App.jsx
@@ -1,30 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup";
 import Login from "./pages/login";
 import Products from "./pages/Products";
 import Branches from "./pages/Branches";
-import { CartProvider } from "./Context/CartContext";
 import { UserProvider } from "./Context/UserContext";
 import AdminDashboard from "./admin/adminDashboard";
 
 export default function App() {
   return (
-
-     
-      <UserProvider>
-        <CartProvider>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/branches" element={<Branches />} />
-            <Route path="/products/:branchId" element={<Products />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-          </Routes>
-        </CartProvider>
-      </UserProvider>
-    
-
-
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/branches" element={<Branches />} />
+        <Route path="/products/:branchId" element={<Products />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </UserProvider>
   );
 }
--- refresh/src/main.jsx
+++ refresh/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './Context/CartContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { CartProvider } from "./Context/CartContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CartProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <CartProvider>
         <App />
-      </BrowserRouter>
-    </CartProvider>
-  </StrictMode>,
-)
+      </CartProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
